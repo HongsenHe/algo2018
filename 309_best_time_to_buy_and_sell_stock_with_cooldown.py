6c3c7b3c62d6d9fdb698fc954c033afc66fce3b6
Hongsen He <hhsen8816@gmail.com>
2019-05-30 19:24:08 -0700

    dp

--- 309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ 309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -4,6 +4,48 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+
+        '''
+        三种操作：买，卖，休，分别DP一下
+        1. 买的前一个操作是休息，如果真买了，利润要减去买入价格也就是当天价格
+        buy[i] = max(res[i-1]-price, buy[i-1]) 比较买还是不买
+        2. 卖的前一个操作是买，如果真卖了，利润要加上当天价格
+        sell[i] = max(buy[i-1]+price, sell[i-1]) 比较卖还是不卖
+        3. 休息的前一个操作可以是买，卖或休息
+        rest[i] = max(buy[i-1], sell[i-1], rest[i-1])
+        但如果买收益肯定小于休息收益，也小于卖的收益。
+        即：buy[i] <= rest[i] <= sell[i]
+        上述方程就是：rest[i] = sell[i-1]
+
+        代入 1 2 方程即：
+        buy[i] = max(sell[i-2]-price, buy[i-1])
+        sell[i] = max(buy[i-1]+price, sell[i-1])
+
+        代码如下：
+        '''
+        
+        if not prices or len(prices) < 2:
+            return 0
+        n = len(prices)
+        buy = [0] * n
+        sell = [0] * n
+        
+        '''
+        buy[i] = max(sell[i-2]-price, buy[i-1])
+        sell[i] = max(buy[i-1]+price, sell[i-1])
+        '''
+        buy[0] = -prices[0]
+        buy[1] = max(0-prices[1], buy[0])
+        sell[1] = max(buy[0]+prices[1], 0)
+        
+        for i in range(2, n):
+            buy[i] = max(sell[i-2]-prices[i], buy[i-1])
+            sell[i] = max(buy[i-1]+prices[i], sell[i-1])
+            
+        return max(buy[n-1], sell[n-1])
+
+
+
         '''
         思路：
         今天卖不卖，今天买不买的问题。。。（不要预测股市）
@@ -19,6 +61,7 @@ class Solution(object):
         看最后一天结束的时候，手里没有股票的最大利润 sell[n-1]
         '''
         
+        '''
         if not prices or len(prices) < 2:
             return 0
         
@@ -33,3 +76,6 @@ class Solution(object):
             buy[i] = max(sell[i-2] - prices[i], buy[i-1])
             sell[i] = max(buy[i-1] + prices[i], sell[i-1])
         return sell[n-1]
+        '''
+
+
