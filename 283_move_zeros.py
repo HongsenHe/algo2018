dab93eb6d04ca34e7dd636d5bd171ed6b9baee10
Hongsen He <hhsen8816@gmail.com>
2019-11-23 12:42:12 -0800

    arr

--- 283_move_zeros.py
+++ 283_move_zeros.py
@@ -6,7 +6,5 @@ class Solution:
         j = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                if i != j:
-                    nums[j] = nums[i]
-                    nums[i] = 0
+                nums[i], nums[j] = nums[j], nums[i]
                 j += 1
