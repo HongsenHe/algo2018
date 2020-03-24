d27eea0fbf9df35fdcc9ef5860ae9195a8181e69
hongsen <hhsen8816@gmail.com>
2020-03-23 20:43:33 -0700

    two pts

--- 015_3_sum.py
+++ 015_3_sum.py
@@ -1,32 +1,37 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums.sort()
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        for x in range(0, len(nums) - 2):
-            # skip duplicated number, as calculated
-            if x and nums[x] == nums[x - 1]:
+        nums.sort()
+        
+        for i in range(len(nums)-2):
+            target = -nums[i]
+            a = i + 1
+            b = len(nums) - 1
+            
+            # skip the duplicated number
+            if i and nums[i] == nums[i-1]:
                 continue
             
-            target = -nums[x]
-            i = x + 1
-            j = len(nums) - 1
-            while i < j:
-                if nums[i] + nums[j] == target:
-                    res.append([nums[x], nums[i], nums[j]])
-                    j -= 1
-                    i += 1
-                    # deduplicating
-                    while i < j and nums[i] == nums[i - 1]:
-                        i += 1
-                    while i < j and nums[j] == nums[j + 1]:
-                        j -= 1
-                elif nums[i] + nums[j] > target:
-                    j -= 1
+            while a < b:
+                if nums[a] + nums[b] > target:
+                    b -= 1
+                elif nums[a] + nums[b] < target:
+                    a += 1
                 else:
-                    i += 1
+                    res.append([nums[i], nums[a], nums[b]])
+
+                    # already found one solution, keep search based on current target!
+                    a += 1
+                    b -= 1
+                    # deduplicating with the previous one, a vs a-1, b vs b+1
+                    while a < b and nums[a] == nums[a-1]:
+                        a += 1
+                    while a < b and nums[b] == nums[b+1]:
+                        b -= 1
+                    
+                    
+            
         return res
-  
+                
+            
+        
