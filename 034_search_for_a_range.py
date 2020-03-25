d23c0a7d5fa51e89d81f7293d2db4466e60346a8
hongsen <hhsen8816@gmail.com>
2020-03-24 23:03:13 -0700

    bs, binary search

--- 034_search_for_a_range.py
+++ 034_search_for_a_range.py
@@ -1,54 +1,46 @@
-class Solution(object):
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return(self.search_left(nums, target), self.search_right(nums, target))
+        
+    
+    def search_left(self, nums, target):
+        idx = -1
         left = 0
         right = len(nums) - 1
-        res = []
         
-        if not nums:
-            return [-1, -1]
-        
-        while left + 1 < right:
-            mid = left + (right-left) / 2
-            if nums[mid] == target:
-                # narrow down by moving right pointer to left side
-                right = mid
-            elif nums[mid] > target:
-                right = mid
+        while left <= right:
+            mid = left + (right - left) // 2
+            # two cases: narrow down by moving the right pointer to the mid
+            # and calculate left - mid side only
+            if nums[mid] >= target:
+                right = mid - 1
             else:
-                left = mid
+                left = mid + 1
                 
-        # find start index
-        if nums[left] == target:
-            res.append(left)
-        elif nums[right] == target:
-            res.append(right)
-        else:
-            return [-1, -1]
-        
-        # reset to find end index
+            # find find the value, unlike the traditional BS, don't return
+            # keep this mid index, this part is doing left - mid, so if
+            # this mid is the only one, then use it. if it's not the left most
+            # just update it.
+            if nums[mid] == target:
+                idx = mid
+                
+        return idx
+    
+    
+    def search_right(self, nums, target):
+        idx = -1
         left = 0
         right = len(nums) - 1
         
-        while left + 1 < right:
-            mid = left + (right-left) / 2
-            if nums[mid] == target:
-                # narrow down by moving left pointer to right side
-                left = mid
-            elif nums[mid] > target:
-                right = mid
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] <= target:
+                left = mid + 1
             else:
-                left = mid
-            
-        if nums[right] == target:
-            res.append(right)
-        elif nums[left] == target:
-            res.append(left)
-        else:
-            return [-1, -1]
+                right = mid - 1
+                
+            if nums[mid] == target:
+                # calculate mid - right side
+                idx = mid
+        return idx
         
-        return res        
--- 153_find_minimum_in_rotated_sorted_array.py
+++ 153_find_minimum_in_rotated_sorted_array.py
@@ -1,18 +1,21 @@
-class Solution(object):
-    def findMin(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
         
         while left < right:
-            mid = left + (right - left) / 2
-            if nums[mid] < nums[right]:
-                right = mid
-            else:
+            mid = left + (right - left) // 2
+            
+            # usually mid data <= right data in sorted list
+            # if so, then the min data is in mid - right
+            if nums[mid] > nums[right]:
+                # +1, as mid is not the min data, mid is already greater than right
+                # so use +1 for skipping the mid position
                 left = mid + 1
+            else:
+                # mid could be equal to right, so do not skip mid
+                # if mid < right, mid could be the min data, anyway do not skip mid
+                right = mid
+                
         return nums[left]
-        
+                
