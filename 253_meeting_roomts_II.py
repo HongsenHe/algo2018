925feb9ec349dd857bfe8693a7ded4e29aeef823
hongsen <hhsen8816@gmail.com>
2020-03-24 19:40:34 -0700

    interval

--- 253_meeting_roomts_II.py
+++ 253_meeting_roomts_II.py
@@ -1,34 +1,26 @@
-# Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
 class Solution:
-    def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: int
-        """
-        
-        '''
-        先按照开始时间排序, 用一个数列指保存每个房间的结束时间，数列大小即房间多少。
-        对于每一个房间，如果开始时间小于数列中最早结束时间，即要开一个新的房间
-        因为人家都还没结束，你就开始了，另起炉灶。这里需要最小堆找到最早结束时间。
-        如果大于等于，证明这个房间还可以加会议，更新结束时间。
-        '''
+    '''
+    先按照开始时间排序, 用一个数列指保存每个房间的结束时间，数列大小即房间多少。
+    对于每一个房间，如果开始时间小于数列中最早结束时间，即要开一个新的房间
+    因为人家都还没结束，你就开始了，另起炉灶。这里需要最小堆找到最早结束时间。
+    如果大于等于，证明这个房间还可以加会议，更新结束时间。
+    '''
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if not intervals:
             return 0
         
-        intervals.sort(key=lambda x :x.start)
+        intervals.sort(key=lambda x :x[0])
+        # 为什么要和最早结束时间比？因为这样可以判断当前会议是否能接上最早结束的会议并且继续使用教室
+        # 如果这都无法满足，另起炉灶比较好。
         heap = [] # min heap, save each room release time
         for interval in intervals:
-            if heap and interval.start >= heap[0]:
+            # 如果当前的会议开始时间3pm大于最早结束时间2pm, 可以继续用这个教室但要更新结束时间
+            if heap and interval[0] >= heap[0]:
                 # update this room release time
-                heapq.heapreplace(heap, interval.end)
+                heapq.heapreplace(heap, interval[1])
             else:
+                # 如果当前的会议开始时间1pm 小于最早结束时间2pm, 则另起炉灶放到heap里
                 # add a new room and set release time
-                heapq.heappush(heap, interval.end)
+                heapq.heappush(heap, interval[1])
                 
         return len(heap)
-        
