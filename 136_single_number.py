5071cb84a9e5198d55cd1c82ffa1e018386f1bba
Hongsen He <hhsen8816@gmail.com>
2020-04-01 20:48:29 -0700

    bit

--- 136_single_number.py
+++ 136_single_number.py
@@ -13,3 +13,16 @@ class Solution(object):
             else:
                 hm[num] = 1
         return list(hm.keys())[0]
+
+
+        O(1) memory
+        res = 0
+        '''
+        XOR thing:
+        a ^ a = 0
+        a ^ 0 = a
+        '''
+        for num in nums:
+            res ^= num
+        return res
+        
