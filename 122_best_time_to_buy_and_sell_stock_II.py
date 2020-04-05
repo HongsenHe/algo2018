78e04de273877267ec34e32d383cae11e26fc5c0
Michael He <michaelhe@hongsen-mbp.local>
2020-04-05 12:47:14 -0700

    30 days chalg

--- 122_best_time_to_buy_and_sell_stock_II.py
+++ 122_best_time_to_buy_and_sell_stock_II.py
@@ -10,3 +10,17 @@ class Solution(object):
                 # 赚钱就跑的小散
                 profit += prices[i] - prices[i-1]
         return profit
+
+
+    # updated at 04052020
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        buy = float('inf')
+        profit = 0
+        
+        for price in prices:
+            if price > buy:
+                profit += price - buy
+            buy = price
+        return profit
