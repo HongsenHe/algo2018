eb514920e84e534c0079f66faac4c8ba9713605b
hongsen <hhsen8816@gmail.com>
2020-05-04 09:35:15 -0700

    bit

---
+++ 476_number_complement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        todo, bit = num, 1
+        while todo:
+            # flip current bit
+            num = num ^ bit
+            # prepare for the next run
+            bit = bit << 1
+            todo = todo >> 1
+        return num
