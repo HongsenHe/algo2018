982937830ec0f7f2e1264ce6f49c9ca95a5a7609
hongsen <hhsen8816@gmail.com>
2021-04-17 19:07:11 -0700

    bs

--- 035_search_insert_position.py
+++ 035_search_insert_position.py
@@ -1,24 +1,24 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left = 0
-        right = len(nums) - 1
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
         
-        while left <= right:
-            mid = left + (right - left) / 2
-            if nums[mid] > target:
-                # should skip mid no need compare, then use left <= right
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            
+            if nums[mid] < target:
+                left = mid
             else:
-                return mid
-        return left
-                
-
-
+                right = mid
         
+        '''
+        经典二分模板，之后要注意判断有几种情况。
+        target在left的左边，就用左指针，
+        target在left和right之间，就用右指针。
+        不然就插入最后一个位置，即len(nums)
+        '''
+        
+        if nums[left] >= target:
+            return left
+        if nums[right] >= target:
+            return right
+        return len(nums)
