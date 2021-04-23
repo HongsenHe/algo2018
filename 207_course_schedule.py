8367a4e8f42217112fdc02d29113b7fe1b29f3df
hongsen <hhsen8816@gmail.com>
2021-04-22 17:23:44 -0700

    bfs

--- 207_course_schedule.py
+++ 207_course_schedule.py
@@ -1,50 +1,68 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        # 两个经典构建图的模板
-        # 1. k = 列出所有点 v = 其孩子节点(list)
-        edges = {i: [] for i in range(numCourses)}
+from collections import deque
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        '''
+        此题本质是在图中找个环。如果有环当然无法先修某个课，造成死循环。
+        难点在于构建图: 点与边的集合，入度的集合。
         
-        # 2. 初始所有点的入度
+        经典BFS模板
+        1. 用collections.deque() 创建queue
+        2. 首先把所有入度为0的点加进queue中。
+        3. 弹出来，并且看他的邻居们的状态，此时邻居的入度要减去1。
+        4. 如果当前邻居的入度是0, 放进queue中继续处理。
+        5. 如果弹出来的个数等于numCourses, 则跑完全程没有环。
+        
+        '''
+        
+        # 初始化构建点的集合, 用邻接表的思想(dict)。key是任意一点，value是这个点的邻居们
+        edges = {i : [] for i in range(numCourses)}
+        
+        # 构建入度的集合，先初始化
         degrees = [0 for i in range(numCourses)]
         
-        # 构建edges: 每个节点之间的关系
-        # 构建degrees: 每个节点的入度
-        for k, v in prerequisites:
-            # 把孩子们加入父节点的list中
-            edges[v].append(k)
-            # 相应节点的入度要增加
-            degrees[k] += 1
+        # 再加入具体点和邻居的关系，并且对于每一个点计算in degree
+        for out_degree, in_degree in prerequisites:
+            edges[in_degree].append(out_degree)
+            degrees[out_degree] += 1
             
-        # 经典BFS
-        import queue
-        queue = queue.Queue(maxsize = numCourses)
+        # 此处开始经典BFS模板
+        queue = deque()
         count = 0
         
-        # 把入度是0 即起始点 加入queue
-        for i in range(numCourses):
-            if degrees[i] == 0:
-                queue.put(i)
-        
-        while not queue.empty():
-            node = queue.get()
-            count += 1 # 弹出来一个，找到了一个
+        # BFS的第一层，先把入度为0的所有点加进去。
+        for course in range(numCourses):
+            if degrees[course] == 0:
+                queue.append(course)
             
-            for each in edges[node]:
-                # 因为当前的节点少了一个父节点（node) 所以入度减一
-                degrees[each] -= 1
+        # always this condition
+        while queue:
+            cur = queue.popleft()
+            '''
+            弹出来一个，则找到一个全新的节点
+            如果有环，比如 0 -> 1, 2 -> 1, 1 -> 2
+            则一开始把0放进queue, 但1的入度并不是0，因为2也指向1
+            此时的count 只是1，但numCourses是3
+            '''
+            count += 1
+            
+            # 对于当前点的所有邻居进行操作，即入度减1
+            for neighbor in edges[cur]:
+                degrees[neighbor] -= 1
                 
-                # 调整之后如果当前节点入度是0，即初始点，放进queue
-                if degrees[each] == 0:
-                    queue.put(each)
-        
-        # 如果构建之后，没有环，那么结果应该和课程数量相等
-        return count == numCourses                
-        
-        
+                # 如果当前点的入度是0，则加入queue中，进行下一层计算。
+                if degrees[neighbor] == 0:
+                    queue.append(neighbor)
+                    
+        return count == numCourses
+                
+                
             
+            
+        
+                    
+                    
+        
+        
         
--- 210_course_schedule_II.py
+++ 210_course_schedule_II.py
@@ -1,62 +1,46 @@
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        
-        # numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
-        
-        # {0: [], 1: [], 2: [], 3: []}
-        edges = {i: [] for i in range(numCourses)}
+from collections import deque
 
-        # [0, 0, 0, 0]
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # 构建邻接表和每个点的入度关系，初始化
+        edges = {i : [] for i in range(numCourses)}
         degrees = [0 for i in range(numCourses)]
+        
+        # 添加数据
+        for out_degree, in_degree in prerequisites:
+            edges[in_degree].append(out_degree)
+            degrees[out_degree] += 1
+        
+        queue = deque()
 
-        # 构造节点之间的关系和计算入度
-        for i, j in prerequisites:
-            # key是父节点，value是孩子节点
-            # {0: [1, 2], 1: [3], 2: [3], 3: []}
-            edges[j].append(i)
-
-            # 每当找到一个点是被指向的，说明他依赖一个别的节点，
-            # 所以就增加一个度。 起始点入度是0
-            # [0, 1, 1, 2]
-            degrees[i] += 1
-
-        # 经典BFS 走起！
-        import queue
-        queue = queue.Queue(maxsize = numCourses)
-
-        for i in range(numCourses):
-            # 先计算所有入度是0的点，意味着他们都是起始点
-            # 放到Queue里面，开始经典BFS套路！
-            if degrees[i] == 0:
-                queue.put(i)
-
+        # 把所有入度为0的节点放进queue, 作为第一层遍历
+        for course in range(numCourses):
+            if degrees[course] == 0:
+                queue.append(course)
+        
         order = []
-
-        while not queue.empty():
-            # 弹出来一个初始点，在此基础上算他的孩子节点
-            node = queue.get()
-            # 因为是初始点，所以可以放到result set里面
-            order.append(node)
-
-            # 计算初始点所有的孩子节点
-            for x in edges[node]:
-                # 因为孩子节点少了一个父亲，就是刚才的初始点
-                # 所以孩子的入度要减去一
-                degrees[x] -= 1
-
-                # 同理在入度减去一之后，如果孩子的入度是0
-                # 那么孩子就是初始点了，也要加进Queue
-                if degrees[x] == 0:
-                    queue.put(x)
-
-        # 方式环的问题
+        
+        # 经典BFS
+        while queue:
+            # 弹出来一个，马上加入结果集或者访问集
+            cur = queue.popleft()
+            order.append(cur)
+            
+            # 开始遍历当前节点的每一个邻居
+            for neighbor in edges[cur]:
+                # 当前邻居的入度都减1，直到为0，即成为起始点，加入queue计算
+                degrees[neighbor] -= 1
+                
+                if degrees[neighbor] == 0:
+                    queue.append(neighbor)
+                    
+        # 判断是否有环，如果没有，则返回order
         if len(order) == numCourses:
             return order
-
+        
         return []
+            
+            
+        
         
