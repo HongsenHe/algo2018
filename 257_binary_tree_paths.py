a8c3c9f61c280b633b4ad60f3f3d57fca843c1c7
hongsen <hhsen8816@gmail.com>
2021-04-27 11:46:03 -0700

    dfs

--- 257_binary_tree_paths.py
+++ 257_binary_tree_paths.py
@@ -1,30 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def binaryTreePaths(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[str]
-        """
-        res = []
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
         if not root:
-            return res
-        self.helper(root, res, str(root.val))
+            return None
+        res = []
+        self.dfs(root, res, [str(root.val)])
         return res
     
-    def helper(self, root, res, each):
-        # exit, as current node is leaf
+    def dfs(self, root, res, path):
         if not root.left and not root.right:
-            res.append(each)
-            return 
+            path_str = '->'.join(path)
+            res.append(path_str)
+            return
         
-        # adding child treeNode, not current root, otherwise init root will be added twice
         if root.left:
-            self.helper(root.left, res, each + "->" + str(root.left.val)) 
+            path.append(str(root.left.val))
+            self.dfs(root.left, res, path)
+            path.pop()
+            
         if root.right:
-            self.helper(root.right, res, each + "->" + str(root.right.val))
+            path.append(str(root.right.val))
+            self.dfs(root.right, res, path)
+            path.pop()
