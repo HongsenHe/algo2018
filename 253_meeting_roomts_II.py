4f953d0f23fc9522ed829b5a3efa48e28b06606e
hongsen <hhsen8816@gmail.com>
2021-05-13 17:26:21 -0700

    heap

--- 253_meeting_roomts_II.py
+++ 253_meeting_roomts_II.py
@@ -1,6 +1,37 @@
 import heapq
 
 class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+
+        # 05132021
+            '''
+        heap 经典应用
+        先按照开始时间排序，确保一个参数(start)是升序的。
+        用min heap来存下课时间。如果当前的开始时间比下课时间早，
+        那就要再开一个房间，并且把当前课程的下课时间存进去。
+        
+        如果开始时间比下课晚，继续等 即可以使用此教室，因为按照开始时间排序了。
+        所以不存在插队现象。
+        同时把正在进行的课程(peak) 弹出来，放入当前课程的结束时间。
+        
+        '''
+        intervals.sort(key=lambda x: x[0])
+        heap = []
+        heapq.heappush(heap, intervals[0][1])
+        room = 1
+        
+        for start, end in intervals[1:]:
+            # 前一个还没下课就开始了，重新开一个房间， 并且更新保留最小ending time作为peak
+            if start < heap[0]:
+                room += 1
+                heapq.heappush(heap, end)
+            else:
+                heapq.heappop(heap)
+                heapq.heappush(heap, end)
+                    
+        return room
+            
+
     '''
     先按照开始时间排序, 用一个数列指保存每个房间的结束时间，数列大小即房间多少。
     对于每一个房间，如果开始时间小于数列中最早结束时间，即要开一个新的房间
