ffe6fe11d2d6b57745b297aade6d1e73b4644663
hongsen <hhsen8816@gmail.com>
2021-05-15 21:36:34 -0700

    tree

--- 145_binary_tree_postorder_traversal.py
+++ 145_binary_tree_postorder_traversal.py
@@ -7,21 +7,35 @@
 
 class Solution:
     def postorder(self, root):
-    res = []
-    if not root:
-        return res
-    
-    sk = [root]
-    
-    while sk:
-        root = sk.pop()
-        res.insert(0, root.val)
-        if root.left:
-            sk.append(root.left)
-        if root.right:
-            sk.append(root.right)
+        res = []
+        if not root:
+            return res
         
-    return res
+        sk = [root]
+            
+        # 05152021
+        '''
+        postorder顺序是 left right root. 最后加入root节点
+        也可以说把root节点放到res list的前面。
+        每次弹出来一个节点即root, 压入stack里。如果有left tree
+        压入stack， 同理right tree
+        
+        此题不能先一天路走到黑，去让root = root.left， 因为即使
+        left tree到底了，还需要有right tree. 都有了才能 插入
+        他们的root. 
+        既然顺序是从上到下，不妨每次访问root的时候就插入res list
+        用反序的方式。
+        '''
+
+        while sk:
+            root = sk.pop()
+            res.insert(0, root.val)
+            if root.left:
+                sk.append(root.left)
+            if root.right:
+                sk.append(root.right)
+            
+        return res
     
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
