d007ffd0f627b70c2d359174e583745711dc19b9
hongsen <hhsen8816@gmail.com>
2021-05-15 21:11:20 -0700

    tree

--- 226_invert_binary_tree.py
+++ 226_invert_binary_tree.py
@@ -20,3 +20,49 @@ class Solution(object):
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+
+    # 05152021
+
+        ************ another solution ************
+
+        if root:
+            # you have to put it in one line
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        
+        return root
+
+
+        ************ another solution ************
+
+        if not root:
+            return
+        
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+        root.left = right
+        root.right = left
+        
+        return root
+
+
+
+
+        ************ another solution ************
+        if not root:
+            return None
+        
+        queue = []
+        queue.append(root)
+        
+        while queue:
+            cur = queue.pop()
+            cur.left, cur.right = cur.right, cur.left
+            
+            if cur.left:
+                queue.append(cur.left)
+            if cur.right:
+                queue.append(cur.right)
+                
+        return root
+            
