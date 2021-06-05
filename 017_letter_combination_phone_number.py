f85031974eebaac52e416a5903c457c9985ff593
hongsen <hhsen8816@gmail.com>
2021-06-05 12:43:23 -0700

    dfs

--- 017_letter_combination_phone_number.py
+++ 017_letter_combination_phone_number.py
@@ -1,3 +1,52 @@
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        
+        # 全新做法 06052021!
+        if not digits:
+            return []
+        
+        res = []
+        self.helper(res, digits, phone, [], 0)
+        
+        return res
+    
+    def helper(self, res, digits, phone, each, start):
+        # 当前子集each个数符合条件，放入结果集里
+        if len(each) == len(digits):
+            each_str = ''.join(each)
+            res.append(each_str)
+            return
+        
+        '''
+        两个数字23，需要用2的abc和3的def配对，则需要一个start pointer
+        不停向前走，即在outer for loop使用start
+        对于每一个数字2的字母集(abc), 需要和下一个字母集(def)配对，经典回溯法
+        ad, ae, af, a这层都结束之后，则以b开头，即bd, be, bf， 以此类推
+        '''
+        
+        for i in range(start, len(digits)):
+            letters = phone[digits[i]]
+            
+            for letter in letters:
+                each.append(letter)
+                self.helper(res, digits, phone, each, i + 1)
+                each.pop()
+        
+        
+            
+
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         phone = {
