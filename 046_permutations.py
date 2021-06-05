9933647a42f64d243c3276dc1fcae1faa5c17636
hongsen <hhsen8816@gmail.com>
2021-06-05 11:48:52 -0700

    dfs

--- 046_permutations.py
+++ 046_permutations.py
@@ -4,14 +4,18 @@ class Solution:
         self.helper(nums, res, [])
         return res
     
-    def helper(self, nums, res, cur):
-        if len(cur) == len(nums):
-            res.append(list(cur))
-            return
+    def helper(self, nums, res, each):
+        # 退出条件就是当前子集长度是输入长度，深拷贝到最终解
+        if len(each) == len(nums):
+            res.append(list(each))
+            return 
         
+        
+        # 可以是任何组合，不涉及start index, 所以直接for loop
+        # 经典回溯：把当前数字加进去，跑下一层，弹出去。如果重复了则继续
         for num in nums:
-            if num in cur:
+            if num in each:
                 continue
-            cur.append(num)
-            self.helper(nums, res, cur)
-            cur.pop()
+            each.append(num)
+            self.helper(nums, res, each)
+            each.pop()
