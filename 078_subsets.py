ab199108daf1191a1479bc94d93f5a34eaa8393e
hongsen <hhsen8816@gmail.com>
2021-06-05 14:39:29 -0700

    dfs

--- 078_subsets.py
+++ 078_subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        self.helper(res, [], 0, nums)
+        self.helper(res, nums, [], 0)
         return res
     
-    def helper(self, res, each, start, nums):
+    def helper(self, res, nums, each, start):
+        # 既然是subset,所以把所有解（树的所有节点）放到结果集中
         res.append(list(each))
         
+        # 举例：以1为开始，逐渐加入2， 3，即 需要start 并且传下去。
+        # 然后以2位开始，但要继续走下去，避免21和12重复。
         for i in range(start, len(nums)):
             each.append(nums[i])
-            self.helper(res, each, i+1, nums)
+            self.helper(res, nums, each, i + 1)
             each.pop()
-            
-        
