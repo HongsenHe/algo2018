cf0c27df7a17a8c35aa214fd7e7d46a2d6a7400a
hongsen <hhsen8816@gmail.com>
2021-06-07 11:50:06 -0700

    tree

--- 235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ 235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,28 +7,18 @@
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
         if not root or not p or not q:
             return root
+        
         while root:
-            if root.val < p.val and root.val < q.val:
-                root = root.right
-            elif root.val > p.val and root.val > q.val:
-                root = root.left
+            if root.val < min(p.val, q.val):
+                # root = root.right
+                return self.lowestCommonAncestor(root.right, p, q)
+            elif root.val > max(p.val, q.val):
+                # root = root.left
+                return self.lowestCommonAncestor(root.left, p, q)
             else:
                 break
+                
         return root
-        # if not root or not p or not q:
-        #     return root
-        # if root.val < p.val and root.val < q.val:
-        #     return self.lowestCommonAncestor(root.right, p, q)
-        # elif root.val > p.val and root.val > q.val:
-        #     return self.lowestCommonAncestor(root.left, p, q)
-        # return root
-        
             
