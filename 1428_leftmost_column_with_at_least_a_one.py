26ad31672a00cdf4b00dbad69abc8635b981492a
hongsen <hhsen8816@gmail.com>
2021-07-20 15:00:15 -0700

    fb

--- 1428_leftmost_column_with_at_least_a_one.py
+++ 1428_leftmost_column_with_at_least_a_one.py
@@ -3,15 +3,46 @@
 # You should not implement it, or speculate about its implementation
 # """
 #class BinaryMatrix(object):
-#    def get(self, x: int, y: int) -> int:
+#    def get(self, row: int, col: int) -> int:
 #    def dimensions(self) -> list[]:
 
 class Solution:
     def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
-        # I have no idea about this question...
-        n,m = binaryMatrix.dimensions();i = n-1;j= m-1
-        while (i>=0 and j>=0):
-            if binaryMatrix.get(i,j)==0 :i-=1
-            else:j-=1
+        rows, cols = binaryMatrix.dimensions()
+        left = 0
+        right = cols - 1
+        '''
+        上二分法模板。 左右指针分别用列的头和尾。
+        确定列的中点后，看每一行的数字。
+        如果中点是1，则舍弃右边部分。因为右边肯定都是1.
+        
+        最后分别看左右指针的每一行。
+        
+        '''
+        
+        while left + 1 < right:
+            mid_col = (left + right) // 2
             
-        return -1 if j == m-1  else j+1
+            flag = False
+            for row in range(rows):
+                if binaryMatrix.get(row, mid_col) == 1:
+                    flag = True
+                    break
+                    
+            if flag:
+                right = mid_col
+            else:
+                left = mid_col
+                
+        for row in range(rows):
+            if binaryMatrix.get(row, left) == 1:
+                return left
+            
+        for row in range(rows):
+            if binaryMatrix.get(row, right) == 1:
+                return right
+            
+        return -1
+                    
+                
+            
