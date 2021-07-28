e721a1408b2a8c300d004e1021dbde3bcb75093e
hongsen <hhsen8816@gmail.com>
2021-07-28 15:54:33 -0700

    hashtable

--- 036_valid_sudoku.py
+++ 036_valid_sudoku.py
@@ -1,3 +1,53 @@
+# 07282021
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = len(board)
+        cols = len(board[0])
+        
+        # verify each row
+        for i in range(rows):
+            visited = set()
+            for j in range(cols):
+                if not self.valid(board, i, j, visited):
+                    return False
+                
+        # verify each col
+        for i in range(cols):
+            visited = set()
+            for j in range(rows):
+                if not self.valid(board, j, i, visited):
+                    return False
+
+        # verify 3x3
+        for i in range(0, rows, 3):
+            for j in range(0, cols, 3):
+                visited = set()
+                for x in range(i, i + 3, 1):
+                    for y in range(j, j + 3, 1):
+                        if not self.valid(board, x, y, visited):
+                            return False
+                        
+        return True
+    
+    def valid(self, board, i, j, visited):
+        num = board[i][j]
+
+        if num == '.':
+            return True
+        
+        num = int(num)
+        if num > 9 or num < 1:
+            return False
+        if num in visited:
+            return False
+        
+        visited.add(num)
+        
+        return True
+                
+
+
+                
 class Solution:
     def isValidSudoku(self, board):
         """
--- 438_find_all_anagrams_in_a_string.py
+++ 438_find_all_anagrams_in_a_string.py
@@ -1,4 +1,32 @@
 class Solution:
+    # 07282021, use list
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        size = len(p)
+        p_list = sorted(list(p))
+        res = []
+        
+        for i in range(len(s) - size + 1):
+            cur_str = s[i: i + size]
+            cur_list = sorted(list(cur_str))
+            if cur_list == p_list:
+                res.append(i)
+                
+        return res
+                
+    # 07282021, use Counter, LTE
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        size = len(p)
+        p_list = Counter(p)
+        res = []
+        
+        for i in range(len(s) - size + 1):
+            cur_str = s[i: i + size]
+            if Counter(cur_str) == p_list:
+                res.append(i)
+                
+        return res
+            
+
     def findAnagrams(self, s: str, p: str) -> List[int]:
         res = []
         m = len(s)
