f5b4787e86210311b773446b30c333d26ae93864
hongsen <hhsen8816@gmail.com>
2022-03-12 18:01:53 -0800

    hashmap

--- 299_bulls_and_cows.py
+++ 299_bulls_and_cows.py
@@ -1,5 +1,48 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
+        secret_map = Counter(list(secret))
+        bull_cnt = 0
+        cow_cnt = 0
+        
+        '''
+        03122022
+        如果猜对了cow，则要从map 删除一个secret值，即secret_map[cur_elem] -= 1
+        但这样会影响猜对bull的结果。因为删除的那个字母可能就是bull. 
+        
+        所以要走两遍，第一遍是只看值一样的，第二遍才是 剩下，值一样但位置不同的。
+        '''
+        
+        # 只看值一样的，走一遍
+        for i in range(len(guess)):
+            cur_elem = guess[i]
+            if cur_elem != secret[i]:
+                continue
+                
+            secret_map[cur_elem] -= 1
+            bull_cnt += 1
+        
+        # 看值不一样的
+        for i in range(len(guess)):
+            cur_elem = guess[i]
+            if cur_elem == secret[i]:
+                continue
+                
+            # 完全不在的，即瞎猜的，略
+            if cur_elem not in secret_map:
+                continue
+                
+            if secret_map[cur_elem] > 0:
+                secret_map[cur_elem] -= 1
+                cow_cnt += 1
+        
+        return str(bull_cnt) + 'A' + str(cow_cnt) + 'B'
+                
+        
+
+
+
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
         dic_s = {}
         dic_g = {}
         A = B = 0
