b24ac7ff159c5c05f314084525e6f47e4a890c19
hongsen <hhsen8816@gmail.com>
2022-03-13 20:44:02 -0700

    heap

--- 347_top_k_frequent_elements.py
+++ 347_top_k_frequent_elements.py
@@ -1,3 +1,50 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        03132022
+        python heap 默认是min heap. 如果维持一个size = k的heap, 
+        则每次都要弹出去，只保留K最多的数字，即把cnt最小的pop掉，用min heap
+        
+        如果不做if pop 操作，即一直添加，heap size = len(nums)
+        最后进行pop K个的时候，则需要把cnt最大的num 弹出来，此时用max heap
+        则在build的时候，需要用-cnt来push 到 heap中去。
+        
+        '''
+        heap = []
+        
+        num_cnt = Counter(nums)
+        
+        for num, cnt in num_cnt.items():
+            heapq.heappush(heap, (cnt, num))
+            
+            if len(heap) > k:
+                heapq.heappop(heap)
+                
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(heap)[1])
+            
+        return res
+    
+#         heap = []
+#         nums_dict = Counter(nums)
+        
+#         for num, cnt in nums_dict.items():
+#             heapq.heappush(heap, (-cnt, num))
+            
+#         res = []
+#         for i in range(k):
+#             res.append(heapq.heappop(heap)[1])
+            
+#         return res
+        
+        
+
+
+
+
 from Queue import PriorityQueue 
 
 class Solution(object):
