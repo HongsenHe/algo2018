ecb6abc7a0035f7410d57dad57fd92f56b3b76fc
hongsen <hhsen8816@gmail.com>
2022-03-17 19:39:46 -0700

    two pointer

--- 015_3_sum.py
+++ 015_3_sum.py
@@ -1,37 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
+        n = len(nums)
+        res = []
         
-        for i in range(len(nums)-2):
+        for i in range(n):
             target = -nums[i]
-            a = i + 1
-            b = len(nums) - 1
+            left = i + 1
+            right = n - 1
             
-            # skip the duplicated number
-            if i and nums[i] == nums[i-1]:
+            # cur = prev, 已经计算过，skip
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
             
-            while a < b:
-                if nums[a] + nums[b] > target:
-                    b -= 1
-                elif nums[a] + nums[b] < target:
-                    a += 1
-                else:
-                    res.append([nums[i], nums[a], nums[b]])
-
-                    # already found one solution, keep search based on current target!
-                    a += 1
-                    b -= 1
-                    # deduplicating with the previous one, a vs a-1, b vs b+1
-                    while a < b and nums[a] == nums[a-1]:
-                        a += 1
-                    while a < b and nums[b] == nums[b+1]:
-                        b -= 1
-                    
-                    
-            
-        return res
+            while left < right:
+                cur_sum = nums[left] + nums[right]
                 
-            
-        
+                if cur_sum == target:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    
+                    # 向回看，cur = prev, skip
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif cur_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+                    
+        return res
