6fea5ba1bb24248afa84812cf817ea0c4844e8b1
hongsen <hhsen8816@gmail.com>
2022-03-18 10:28:29 -0700

    two pointer

--- 454_4sum_II.py
+++ 454_4sum_II.py
@@ -3,6 +3,13 @@ class Solution:
         hm = {}
         res = 0
         
+        '''
+        一共4个tuple, 每个tuple有两个数字，排列组合一下，任意两个tuple就有4中方案，即2x2
+        即, tuple_1_1 * tuple_2_1, tuple_1_2 * tuple_2_1, ...
+        构成的4种方案就是4个sum, 从剩下的两个tuple里找答案，经典问题开始。。。
+        
+        '''
+        
         for a in A:
             for b in B:
                 hm[a + b] = hm.get((a + b), 0) + 1
