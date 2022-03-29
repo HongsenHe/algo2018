ff8e681eff4c256c6f06503e2a01039c30bf6ee2
hongsen <hhsen8816@gmail.com>
2022-03-29 12:35:27 -0700

    dfs

--- 543_diameter_of_binary_tree.py
+++ 543_diameter_of_binary_tree.py
@@ -11,6 +11,17 @@ class Solution:
         '''
         每次计算当前root的左右子树的长度之和和候选结果res比较
         dfs 不断更新res 并且更新当前node的深度
+
+        因为答案并不一定经过根节点，所以要计算每个节点左右子树之和
+        并且和一个全局变量比较。
+
+        标准dfs问题，如果当前节点是None， 则返回0，
+        分别求左右节点的长度，记得此函数是对于root来说，
+        最大长度是多少，即max(left_res, right_res) + 1 (root)
+        left_res = dfs(root.left)
+        right_res = dfs(root.right)
+
+        但其中要维持一个全局变量来找题目中的答案。
         '''
         def dfs(node):
             if not node: return 0
