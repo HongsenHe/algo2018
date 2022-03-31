f969d17908db2b62289974b4f17212e524079ba0
hongsen <hhsen8816@gmail.com>
2022-03-31 11:31:49 -0700

    bst dfs

--- 270_closest_binary_search_tree_value.py
+++ 270_closest_binary_search_tree_value.py
@@ -9,6 +9,14 @@ class Solution:
     def closestValue(self, root: TreeNode, target: float) -> int:
         diff = float('inf')
         res = 0
+
+        '''
+        如果target比当前root小，则root = root.left
+        因为如果去右子树，则距离比左子树更大，但也可能root距离target更近
+        所以也需要一个全局变量打擂台。
+        
+        '''
+
         while root:
             if abs(target-root.val) < diff:
                 diff = abs(target-root.val)
