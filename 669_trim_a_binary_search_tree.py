075b1e2059351e2cf44d5221a2e3c8348ddf89d1
hongsen <hhsen8816@gmail.com>
2022-03-31 16:37:05 -0700

    bst dfs

--- 669_trim_a_binary_search_tree.py
+++ 669_trim_a_binary_search_tree.py
@@ -1,28 +1,31 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def trimBST(self, root, L, R):
-        """
-        :type root: TreeNode
-        :type L: int
-        :type R: int
-        :rtype: TreeNode
-        """
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        '''
+        几种基本情况
+        1. 如果当前的val < low, 说明左子树都可以砍掉，因为root.left < root，即向右走
+        2. 如果当前的val > high, 说明右子树都可以砍掉，root.right > root，向左走
+
+        如果not root, return就好
+        分治法，分别求root.left 和root.right, 最后返回当前root在trim之后的结果
+        
+        '''
+        
         if not root:
             return
-        if root.val < L:
+        if root.val < low:
             # too small, should go right
-            return self.trimBST(root.right, L, R)
-        if root.val > R:
+            return self.trimBST(root.right, low, high)
+        if root.val > high:
             # too big, should go left
-            return self.trimBST(root.left, L, R)
+            return self.trimBST(root.left, low, high)
         
         # trimming,,,and get result
-        root.left = self.trimBST(root.left, L, R)
-        root.right = self.trimBST(root.right, L, R)
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
         return root
