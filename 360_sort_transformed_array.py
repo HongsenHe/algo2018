1ccb379b2313b284dacefced9df2dadf2af01e55
hongsen <hhsen8816@gmail.com>
2022-05-10 21:06:33 -0700

    arr

---
+++ 360_sort_transformed_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
+        res = []
+        for num in nums:
+            res.append(a * num * num + b * num + c)
+            
+        return sorted(res)
+            
